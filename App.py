90237c69b8d48f0b0d0b053fa3aabbae24c359d2
Yamini26284 <yamini26284@gmail.com>
2025-07-14 15:11:40 +0000

    final changes

--- App.py
+++ App.py
@@ -1,3 +1,20 @@
 import pandas as pd
 import pickle as pk
 import streamlit as st
+
+model = pk.load(open('/workspaces/Bengaluru-House-Prediction-using-Regression/House_Prediction.pkl','rb'))
+
+st.header('Banglore House Prices Predictor')
+
+data = pd.read_csv('/workspaces/Bengaluru-House-Prediction-using-Regression/Cleaned_data.csv')
+loc = st.selectbox('Choose the location',data['location'].unique())
+sqft = st.number_input('Enter total sqft')
+beds = st.number_input('Enter No of bedrooms')
+bath = st.number_input('Enter No of bathrooms')
+balc = st.number_input('Enter No of balconies')
+
+input = pd.DataFrame([[loc,sqft,beds,bath,balc]],columns=['location','total_sqft','bath','balcony','bedrooms'])
+if st.button('Predict Price'):
+    output = model.predict(input)
+    out_str = 'Price of the House (In Lakhs) ' + str(int(output[0]*100000))
+    st.success(out_str)
